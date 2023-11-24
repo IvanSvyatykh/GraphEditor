68e31342f3e865b23c21ec356a0b2abecdf83669
Ivan Svyatykh <Zlativansvyat@mail.ru>
2023-11-24 16:10:37 +0500

    сделал поиск минимального пути. Логгер готов

--- GraphEditor/Model/Algorithm/DijkstraAlgorithm.cs
+++ GraphEditor/Model/Algorithm/DijkstraAlgorithm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GraphEditor.Model.Loggers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,17 +12,70 @@ namespace Model.Graph
         private Graph _graph;
         private Dictionary<string, List<Tuple<int, string>>> _matrix;
         private DijkstraLogger _logger;
+        private Dictionary<string, bool> _visited;
+        private Dictionary<string, int> _distance;
         public DijkstraAlgorithm(Dictionary<string, List<Tuple<int, string>>> matrix)
         {
-            _logger = new DijkstraLogger();
+            _graph = new Graph();
             _matrix = matrix;
             Helper.TranslateToGraphWithWeights(_matrix, _graph);
         }
 
         public DijkstraLogger StartAlgorithm(string startNode, string destNode)
         {
+            _logger = new DijkstraLogger();
+            _distance = new Dictionary<string, int>();
+            _visited = new Dictionary<string, bool>();
+            _graph.Names.ToList().ForEach(name => { _visited.Add(name, false); _distance.Add(name, int.MaxValue); });
 
+            if (_distance.ContainsKey(startNode))
+            {
+                _distance[startNode] = 0;
+            }
+            else
+            {
+                throw new ArgumentException($"Узла с именем {startNode} не существует.");
+            }
+
+            Helper.CheckGraph(_graph);
+
+            _logger.AddLog(null, null, $"Начинаем поиск пути из узла {startNode} в {destNode}, пути до всех вершин кроме {startNode} стоят бесконечность. Путь до узла {startNode} 0", 0);
+            DijkstraAlgorithmStart();
+            _logger.AddLog(null, null, $"Обошли все вершины, путь из вершины {startNode} в {destNode} состовялет {_distance[destNode]} ", 0);
             return _logger;
+        }
+
+
+        private void DijkstraAlgorithmStart()
+        {
+            while (_visited.Any(x => !x.Value))
+            {
+                string currentNodeName = _distance.Where(y => !_visited[y.Key]).MinBy(x => x.Value).Key;
+                _logger.AddLog(_graph.GetNodeByName(currentNodeName), null, $"Берем вершину из еще не пройденных путь до которой стоит меньше всего, это вершина {currentNodeName}.", 1);
+
+                foreach (GraphEdge edge in _graph.GetNodeByName(currentNodeName).Edges)
+                {
+                    if (_visited[currentNodeName])
+                    {
+                        _logger.AddLog(edge.GetOtherNode(_graph.GetNodeByName(currentNodeName)), edge,
+                           $"Путь до вершины {edge.GetOtherNode(_graph.GetNodeByName(currentNodeName)).Name} через {currentNodeName} не возможен, так как она уже пройдена.", 1);
+                    }
+                    else if (_distance[edge.GetOtherNode(_graph.GetNodeByName(currentNodeName)).Name] > _distance[currentNodeName] + edge.Weight)
+                    {
+                        _logger.AddLog(edge.GetOtherNode(_graph.GetNodeByName(currentNodeName)), edge,
+                            $"Путь до вершины {edge.GetOtherNode(_graph.GetNodeByName(currentNodeName)).Name} через {currentNodeName} стоит {_distance[currentNodeName] + edge.Weight}, это меньше чем текущеe значение {_distance[edge.GetOtherNode(_graph.GetNodeByName(currentNodeName)).Name]}", 1);
+                        _distance[edge.GetOtherNode(_graph.GetNodeByName(currentNodeName)).Name] = _distance[currentNodeName] + edge.Weight;
+                    }
+                    else
+                    {
+                        _logger.AddLog(edge.GetOtherNode(_graph.GetNodeByName(currentNodeName)), edge,
+                           $"Путь до вершины {edge.GetOtherNode(_graph.GetNodeByName(currentNodeName)).Name} через {currentNodeName} стоит {_distance[currentNodeName] + edge.Weight}, это больше чем текущеe значение {_distance[edge.GetOtherNode(_graph.GetNodeByName(currentNodeName)).Name]}, ничего не меняем", 1);
+                    }
+                }
+
+                _logger.AddLog(_graph.GetNodeByName(currentNodeName), null, $"Обошли всех сосодей из вершины {currentNodeName}, помечаем ее как пройденную.", 2);
+                _visited[currentNodeName] = true;
+            }
         }
     }
 }
--- GraphEditor/Model/Loggers/DijkstraLogger.cs
+++ GraphEditor/Model/Loggers/DijkstraLogger.cs
@@ -8,11 +8,21 @@ namespace Model.Graph
 {
     public class DijkstraLogger
     {
-    
+        // 0 - ничего не делаем и не выделяем лог просто с сообщением
+        // 1 - пометить как вершину из которой происходит обход соседей(временным цветом)
+        // 2 - пометить вершину как пройденную навсегда (каким-то ярким цветом типо красного)
+        public List<Tuple<GraphNode, GraphEdge, string, byte>> Visited { get; private set; }
 
-        public DijkstraLogger() 
+        public DijkstraLogger()
         {
-
+            Visited = new List<Tuple<GraphNode, GraphEdge, string, byte>>();
         }
+
+
+        public void AddLog(GraphNode node, GraphEdge edge, string comment, byte flag)
+        {
+            Visited.Add(Tuple.Create(node, edge, comment, flag));
+        }
+
     }
 }
